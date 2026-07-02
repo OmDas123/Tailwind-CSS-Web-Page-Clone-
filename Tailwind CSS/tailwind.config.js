bd548f582aa039076e73f0ab251177d1d4f5a465
Om Das <omdas7711@gmail.com>
2026-07-02 20:27:51 +0530

    Add files via upload

---
+++ Tailwind CSS/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Make sure this path matches where your HTML files are saved!
+  content: ["./dist/*.{html,js}", "./*.html"], 
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
